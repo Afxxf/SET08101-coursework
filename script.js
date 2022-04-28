e8034580e1f14bead5d86ac6d8932923385c15da
FinlayU <31289993+Afxxf@users.noreply.github.com>
2022-04-28 14:25:24 +0100

    Add files via upload
    removed duplicate code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// initialise array containing question info
 var questions = [
   {
     question: "Scotland's capital Edinburgh is famous for its castle that looks over the city. But when did constuction start on the castle?",
@@ -100,6 +101,7 @@ for(var counter=0; counter<questions.length; counter++){
     questions[randomIndex], questions[index]];
 }
 
+// initialise variables to hold elements
 var quiz = document.getElementById('quiz');
 var evaluate = document.getElementById('submit');
 var results = document.getElementById('results');
@@ -119,17 +121,16 @@ document.getElementById('restart').onclick = function(){
       for(var counter=0; counter<questions.length; counter++){
 
         var allAnswers = ''
-        // for each available answer...
-        for(letter in questions[counter].answers){
+        for(option in questions[counter].answers){
           // add potential answers
           allAnswers = allAnswers + (
-            '<input type="radio" name="id'+counter+'" value="'+letter+'">'
-              + questions[counter].answers[letter]
+            '<input type="radio" name="id'+counter+'" value="'+option+'">'
+              + questions[counter].answers[option]
             + '<br>'
           );
         }
         var currentDisplayImage = "<img src="+ questions[counter].id+'.jpg'+" width='350px' alt='image"+counter+"'><br>";
-        // add output
+        // add html to the page
         document.getElementById('quiz').innerHTML += (
           '<div id="quizSection">' + currentDisplayImage + '<h4 style="text-align: center">Question '+(counter+1)+'</h4> <p class="question" style="text-align:center;">'
           + questions[counter].question + '</p><br>'
@@ -140,34 +141,38 @@ document.getElementById('restart').onclick = function(){
     }
 
     function allAnswersShow(questions, quiz, results){
+      // initialise relevant variables
       var answerContainers = quiz.querySelectorAll('.answers');
       var correctAnswerIdList = []
       var userAnswer = '';
       var numCorrect = 0;
-      //
+      // get submitted answers
       for(var id=0; id<questions.length; id++){
         // find selected answer
         userAnswer = (answerContainers[id].querySelector('input[name=id' + id +']:checked')|| '').value;
         if(userAnswer===questions[id].correctAnswer){
           numCorrect = numCorrect + 1;
+          // if answer is correct add it to the list of correct answers for use on results page
           correctAnswerIdList.push(questions[id].id)
         }
       }
+      // set key and value session storage
       sessionStorage.setItem('questionID', correctAnswerIdList);
       var seconds = document.getElementById('seconds').textContent;
       sessionStorage.setItem('seconds', seconds);
       var minutes = document.getElementById('minutes').textContent;
       sessionStorage.setItem('minutes', minutes);
+      // direct user to results page
       window.location.href = "results.html";
     }
 
     display_questions(questions, quiz);
-
-    // on submit, show results
+    // on click of button, evalute all questions
     evaluate.onclick = function(){
       allAnswersShow(questions, quiz, results);
     }
     for (let id = 0; id < 8; id++) {
+      // for every button add an onclick event to show single result
       document.getElementById(id).onclick = function(){
         getSingleAnswer(questions, quiz, results, id)
       }
@@ -177,17 +182,21 @@ document.getElementById('restart').onclick = function(){
     var answerContainers = quiz.querySelectorAll('.answers');
     var userAnswer = '';
     var numCorrect = 0;
+
     userAnswer = (answerContainers[id].querySelector('input[name=id' + id +']:checked')|| '').value;
 
     if(userAnswer===questions[id].correctAnswer){
-      numCorrect = numCorrect + 1;
+      // style accordingly
       document.getElementById(id).style.color = 'green';
       document.getElementById(id).style.background = '#d0ece6';
+      // play relevant sound
       document.getElementById('audio-correct').play();
     }
     else{
+      // stlye accordingly
       document.getElementById(id).style.color = '';
       document.getElementById(id).style.background = '#be4f62';
+      // play revelant sound
       document.getElementById('audio-incorrect').play();
       }
     }
